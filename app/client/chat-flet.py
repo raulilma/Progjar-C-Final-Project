bbde25ee48fba9b3699b87728a83d43e35f6c4ad
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-06-12 20:52:10 +0700

    add: client flet - create group function

--- app/client/chat-flet.py
+++ app/client/chat-flet.py
@@ -8,6 +8,105 @@ TARGET_IP = os.getenv("SERVER_IP") or "127.0.0.1"
 TARGET_PORT = os.getenv("SERVER_PORT") or "8889"
 ON_WEB = os.getenv("ONWEB") or "0"
 
+class ChatList(ft.Container):
+    def __init__(self, page, users, from_user):
+        super().__init__()
+        for value in users.values():
+            print(value["username"])
+        self.content = ft.Column(
+            [
+                ft.ListTile(
+                    leading=ft.Icon(ft.icons.PERSON),
+                    title=ft.Text(f"{value['username']}"),
+                    on_click=lambda _: page.go(f"/private/{value['username']}"),
+                )
+                for value in users.values()
+            ],
+        )
+
+        self.padding = ft.padding.symmetric(vertical=10)
+
+
+class ChatRoom:
+    def __init__(self, page, cc, from_user, to_user):
+        self.chat = ft.TextField(
+            label="Write a message...",
+            autofocus=True,
+            expand=True,
+            on_submit=self.send_click,
+        )
+        self.lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self.send = ft.IconButton(
+            icon=ft.icons.SEND_ROUNDED,
+            tooltip="Send message",
+            on_click=self.send_click,
+        )
+        self.file_picker = ft.FilePicker(on_result=self.upload_files, on_upload=self.upload_server)
+        self.file_pick = ft.IconButton(
+            icon=ft.icons.UPLOAD_FILE_ROUNDED,
+            tooltip="Send file",
+            on_click=self.on_pick_file,
+        )
+        self.page = page
+        self.cc = cc
+        self.from_user = from_user
+        self.to_user = to_user
+        self.page.pubsub.subscribe(self.on_chat)
+
+    def on_pick_file(self, __e__):
+        self.page.overlay.append(self.file_picker)
+        self.page.update()
+        self.file_picker.pick_files(allow_multiple=True)
+
+    def send_click(self, __e__):
+        if not self.chat.value:
+            self.chat.error_text = "Please enter message"
+            self.page.update()
+        else:
+            command = f"send {self.to_user} {self.chat.value}"
+            server_call = self.cc.proses(command)
+            self.lv.controls.append(ft.Text("To {}: {}".format(self.to_user, self.chat.value)))
+
+            if "sent" in server_call:
+                self.page.pubsub.send_all(self.chat.value)
+
+            self.chat.value = ""
+            self.chat.focus()
+            self.page.update()
+
+    def on_chat(self, message):
+        check_inbox = json.loads(self.cc.inbox())
+        self.lv.controls.append(ft.Text("From {}: {}".format(check_inbox[self.to_user][0]['msg_from'], check_inbox[self.to_user][0]['msg'])))
+        self.page.update()
+
+    # file picker and uploads
+    def upload_files(self, e:ft.FilePickerResultEvent):
+        upload_list = []
+        if self.file_picker.result != None and self.file_picker.result.files != None:
+            for f in self.file_picker.result.files:
+                # print(self.page.get_upload_url(f.name, 600),)
+                upload_list.append(
+                    ft.FilePickerUploadFile(
+                        f.name,
+                        upload_url=self.page.get_upload_url(f.name, 600),
+                    )
+                )
+            self.file_picker.upload(upload_list)
+    
+    def upload_server(self, e:ft.FilePickerUploadEvent):
+        if(e.progress == 1):
+            command = f"sendfile {self.to_user} app\\client\\upload\\{e.file_name}"
+            print(command)
+            server_call = self.cc.proses(command)
+            print(server_call)
+            self.lv.controls.append(ft.Text("To {}: Berhasil mengirim file {}".format(self.to_user, e.file_name)))
+
+            if "sent" in server_call:
+                self.page.pubsub.send_all(self.chat.value)
+
+            self.chat.value = ""
+            self.chat.focus()
+            self.page.update()
 
 def main(page):
     cc = ChatClient()
@@ -18,8 +117,6 @@ def main(page):
     def login_dialog():
         nonlocal is_login
         global signin
-        global logouttologin
-        global changeto_logout
 
         def register(e):
             page.dialog.title=ft.Text(
@@ -44,7 +141,6 @@ def main(page):
 
             page.update()
 
-
         page.dialog = ft.AlertDialog(
             open=not is_login,
             modal=True,
@@ -59,6 +155,69 @@ def main(page):
             actions_alignment="end",
         )
     
+    def creategroup_dialog():
+        global creategroup
+        
+        def creategroup(e):
+            page.dialog.title=ft.Text(
+                "Create Group", style=ft.TextThemeStyle.TITLE_MEDIUM
+            )
+            page.dialog.actions=[
+                ft.ElevatedButton("Submit!" ,width=100,height=30, on_click=creategroup_click)
+            ]
+            page.dialog.content=ft.Column([groupname, grouppassword], tight=True)
+            page.update()
+
+        page.dialog = ft.AlertDialog(
+            open=not is_login,
+            modal=True,
+            title=ft.Text(
+                "Create Group", style=ft.TextThemeStyle.TITLE_MEDIUM
+            ),
+            content=ft.Column([groupname, grouppassword], tight=True),
+            actions=[
+                ft.ElevatedButton("Submit!",width=100,height=30, on_click=creategroup_click)
+            ],
+            actions_alignment="end",
+        )
+    
+    def creategroup_click(e):
+        if not groupname.value:
+            groupname.error_text = "Enter a valid groupname."
+            groupname.update()
+        else :
+            groupname.error_text = ""
+            groupname.update()
+
+        if not grouppassword.value:
+            grouppassword.error_text = "You need to enter the group password."
+            grouppassword.update()
+        else :
+            grouppassword.error_text = ""
+            grouppassword.update()
+
+        if groupname.value != "" and grouppassword.value != "":
+            newgroup = cc.addgroup(groupname.value, grouppassword.value)
+
+            if "Error" in newgroup:
+                groupname.error_text = "Register Failed :("
+                groupname.update()
+
+            else:
+                groupname.value = ""
+                grouppassword.value = ""
+                groupname.error_text = ""
+                grouppassword.error_text = ""
+                page.update()
+                page.dialog.open = False
+                
+                
+            page.update()
+    
+    def open_creategroup(e):
+        creategroup_dialog()
+        page.update()
+        
     def logout_click(e):
         is_login = False
         cc.logout()
@@ -158,6 +317,15 @@ def main(page):
     name = ft.TextField(label="Name", autofocus=True)
     country = ft.TextField(label="Country", autofocus=True)
     
+    groupname = ft.TextField(label="Group Name", autofocus=True)
+    grouppassword = ft.TextField(
+        label="Group Password",
+        password=True,
+        can_reveal_password=True,
+        autofocus=True,
+        on_submit=creategroup_click,
+    )
+    
     login_dialog()
 
     def btn_click(e):
@@ -175,9 +343,6 @@ def main(page):
     lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
     cmd = ft.TextField(label="Your command")
 
-    # page.add(lv)
-    # page.add(cmd, ft.ElevatedButton("Send", on_click=btn_click))
-    # Menu buttons grid for main menu
     def menu_buttons():
         return ft.GridView(
             spacing=10,
@@ -225,7 +390,7 @@ def main(page):
                             horizontal_alignment=ft.CrossAxisAlignment.CENTER
                         ),
                         width=250,
-                        on_click=lambda _: page.go("/chat"),
+                        on_click=open_creategroup,
                         style=ft.ButtonStyle(
                             shape={ft.MaterialState.DEFAULT: ft.RoundedRectangleBorder(radius=2)}
                         )
